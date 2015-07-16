3977cb1712a71c826a4f363eea8b0526df58bd52
Rhea Goel <goelrhea1992@gmail.com>
2015-07-16 14:13:25 -0700

    Update SaveImageFromUrl.java

--- Preprocessing/SaveImageFromUrl.java
+++ Preprocessing/SaveImageFromUrl.java
@@ -12,53 +12,41 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class SaveImageFromUrl {
-
 	public static void main(String[] args) throws Exception {
-		
 		FileInputStream fstream = new FileInputStream("database.txt");
-		
 		DataInputStream in = new DataInputStream(fstream);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		String strLine;
 		int i = 1;
-		//ArrayList<String> urls = new ArrayList<String>();
-		while ((strLine = br.readLine()) != null)   {
-		   
-		  String[] parts = strLine.split(" |\\t");
-		  //urls.add(parts[3]);
-		  System.out.println(parts[3]);
-		  saveImage(parts[3], parts[0] + "_" +parts[1] + "_" + parts[2] +".jpg");
-		  
-		  i++;  
+		while ((strLine = br.readLine()) != null) {
+			String[] parts = strLine.split(" |\\t");
+		  	System.out.println(parts[3]);
+		  	saveImage(parts[3], parts[0] + "_" +parts[1] + "_" + parts[2] +".jpg");
+		  	i++;  
 		}
 	}
 
 	public static void saveImage(String imageUrl, String destinationFile) throws IOException {
 		try {
-		URL url = new URL(imageUrl);
-		//long start = System.currentTimeMillis();
-		long end = System.currentTimeMillis() + 30000;
-		
-		while(System.currentTimeMillis() < end)
-		{
-		InputStream is = url.openStream();
-		OutputStream os = new FileOutputStream(destinationFile);
-
-		byte[] b = new byte[2048];
-		int length;
-
-		while ((length = is.read(b)) != -1) {
-			os.write(b, 0, length);
+			URL url = new URL(imageUrl);
+			long end = System.currentTimeMillis() + 30000;
+			
+			while(System.currentTimeMillis() < end)
+			{
+				InputStream is = url.openStream();
+				OutputStream os = new FileOutputStream(destinationFile);
+				byte[] b = new byte[2048];
+				int length;
+				while ((length = is.read(b)) != -1) {
+					os.write(b, 0, length);
+				}
+				is.close();
+				os.close();
+			} 
 		}
-		is.close();
-		os.close();
-		} //end of system while
-		}
-		catch(Exception e)
-		{
+		catch(Exception e) {
 			System.out.println(e);
 			return;
 		}
-		
 	}
 }
